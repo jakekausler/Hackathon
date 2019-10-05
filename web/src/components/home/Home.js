f3fb90785f2afd9c03386359311ee5d92c21200a
jakekausler <jakekausler@yahoo.com>
2019-10-05 13:19:54 -0400

    Add links to home components

--- web/src/components/home/Home.js
+++ web/src/components/home/Home.js
@@ -3,21 +3,21 @@ import './Home.css';
 
 import Header from './../header/Header'
 import Footer from '../footer/Footer'
-// import MapPanel from './mapPanel/MapPanel'
-// import Step1 from './step1/Step1'
-// import Step2 from './step2/Step2'
-// import Step3 from './step3/Step3'
+import MapPanel from './../map/Map'
+import Step1 from './step1/Step1'
+import Step2 from './step2/Step2'
+import Step3 from './step3/Step3'
 // import About from './about/About'
 
 function Home() {
   return (
     <div className="App">
       <Header />
-      {/*<MapPanel />
+      <Map />
       <Step1 />
       <Step2 />
       <Step3 />
-      <About />*/}
+      {/* <About />*/}
       <Footer />
     </div>
   );
