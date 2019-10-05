9c2cf153382e90d59798674dec86278b7a5fff5a
MorganHuegel <mhue2910@gmail.com>
2019-10-05 17:16:27 -0400

    created Assembler Profile component

--- web/src/components/pointPage/PointPage.js
+++ web/src/components/pointPage/PointPage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './PointPage.css';
+
+import AffinityPoints from './affinityPoints/AffinityPoints';
+import AssemblerProfile from './assemblerProfile/AssemblerProfile';
+import ProjectDetails from './projectDetails/ProjectDetails';
+import ProjectManagement from './projectManagement/ProjectManagement';
+import ProjectStatus from './projectStatus/ProjectStatus';
 
 export default function PointPage () {
   return (
     <div>
-      <p>Assembl(i) Point Page</p>
+      <ProjectDetails />
+      <AssemblerProfile />
+      <ProjectStatus />
+      <AffinityPoints />
+      <ProjectManagement />
     </div>
   )
 }
