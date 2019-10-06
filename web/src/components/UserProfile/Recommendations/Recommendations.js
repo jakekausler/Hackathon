26be7ae7e9d81a140b391de1b2fa452fb780a8f2
Briana Craig <brianascraig@gmail.com>
2019-10-06 16:19:28 -0400

    updated Recommendations

--- web/src/components/UserProfile/Recommendations/Recommendations.js
+++ web/src/components/UserProfile/Recommendations/Recommendations.js
@@ -5,7 +5,7 @@ import './Recommendations.css'
 export default function Recommendations() {
     return (
         <div className="Recommendations">
-
+            <h3>Recommendations</h3>
         </div>
     );
 
