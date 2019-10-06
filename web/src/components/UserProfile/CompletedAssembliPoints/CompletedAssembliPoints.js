1872329b845d42a264255577ab951d60b0004f00
Briana Craig <brianascraig@gmail.com>
2019-10-06 15:56:04 -0400

    added UserProfile component

---
+++ web/src/components/UserProfile/CompletedAssembliPoints/CompletedAssembliPoints.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './CompletedAssembliPoints.css'
+
+export default function CompletedAssembliPoints() {
+    return (
+        <div className="CompletedAssembliPoints">
+
+        </div>
+    );
+
+}
